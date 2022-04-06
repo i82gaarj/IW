86fc4dc2f9e29fc672152aa73e63c5e6fd61e1af
Jaime GA <i82gaarj@uco.es>
2022-04-06 10:24:05 +0000

    Añadidos métodos para obtener obra y reporte por ID (incompletos)

--- Studio Classico/src/main/java/es/uco/iw/controller/PieceController.java
+++ Studio Classico/src/main/java/es/uco/iw/controller/PieceController.java
@@ -1,9 +1,43 @@
 package es.uco.iw.controller;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import es.uco.iw.model.Piece;
+import es.uco.iw.model.User;
+import es.uco.iw.model.UserType;
 
 public class PieceController {
-	public Piece getPieceByID(int pieceID) {
-		return null;
+	public Piece getPieceByID(int id) {
+		DBController dbController = new DBController();
+		Piece piece = null;
+		try {
+			Connection con = dbController.getConnection();
+	        PreparedStatement ps = con.prepareStatement("SELECT title, author, year, duration, type, uploader, scorepath, ndownloads, nvisits, uploaddate FROM users WHERE ID = ?");
+	        ps.setInt(1, id);
+	        ResultSet rs = ps.executeQuery();
+	        String title = null, author = null, scorepath = null;
+	        int uploaderID = -1, year = -1, duration = -1, pieceTypeValue = -1, nDownloads = -1, nVisits = -1;
+	        Date uploadDate = null;
+	        if(rs.next()) {
+	        	title = rs.getString(1);
+	        	author = rs.getString(2);
+	        	year = rs.getInt(3);
+	        	duration = rs.getInt(4);
+	        	pieceTypeValue = rs.getInt(5);
+	        	uploaderID = rs.getInt(6);
+	        	scorepath = rs.getString(7);
+	        	nDownloads = rs.getInt(8);
+	        	nVisits = rs.getInt(9);
+	        	uploadDate = rs.getDate(10);
+	        }
+	        con.close();
+	    }catch(SQLException e) {
+	        System.out.println(e);
+	    }
+		return piece;
 	}
 }
--- Studio Classico/src/main/java/es/uco/iw/controller/ReportController.java
+++ Studio Classico/src/main/java/es/uco/iw/controller/ReportController.java
@@ -1,9 +1,40 @@
 package es.uco.iw.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import es.uco.iw.model.Piece;
 import es.uco.iw.model.Report;
+import es.uco.iw.model.User;
+import es.uco.iw.model.UserType;
 
 public class ReportController {
-	public Report getReportByID(int reportID) {
-		return null;
+	public Report getReportByID(int id) {
+		DBController dbController = new DBController();
+		Report report = null;
+		try {
+			Connection con = dbController.getConnection();
+	        PreparedStatement ps = con.prepareStatement("SELECT user, piece, description FROM reports WHERE ID = ?");
+	        ps.setInt(1, id);
+	        ResultSet rs = ps.executeQuery();
+	        int userID = -1;
+	        int pieceID = -1;
+	        String description = null;
+	        if(rs.next()) {
+	        	userID = rs.getInt(1);
+	        	pieceID = rs.getInt(2);
+	        	description = rs.getString(3);
+	        }
+	        UserController userController = new UserController();
+	        PieceController pieceController = new PieceController();
+	        User user = userController.getUserByID(userID);
+	        Piece piece = pieceController.getPieceByID(pieceID);
+	        con.close();
+	    }catch(SQLException e) {
+	        System.out.println(e);
+	    }
+		return report;
 	}
 }
