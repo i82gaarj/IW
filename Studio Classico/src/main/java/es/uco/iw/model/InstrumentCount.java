5037f16d83b1e4c5ff35bdc166dbd10dd2133d73
Jaime García <jaimegarcia1423@gmail.com>
2022-04-19 13:08:05 +0200

    Creada clase InstrumentCount. Renombrado CustomerBean a UserBean.

---
+++ Studio Classico/src/main/java/es/uco/iw/model/InstrumentCount.java
@@ -0,0 +1,22 @@
+package es.uco.iw.model;
+
+public class InstrumentCount {
+	private int count;
+	private Instrument instrument;
+	
+	public int getCount() {
+		return count;
+	}
+	
+	public Instrument getInstrument() {
+		return instrument;
+	}
+	
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public void setInstrument(Instrument instrument) {
+		this.instrument = instrument;
+	}
+}
